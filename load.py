402519130c5023b06937e006797e908519273b51
paledega <38860643+paledega@users.noreply.github.com>
2022-04-04 01:42:19 +0300

    Update load.py

--- load.py
+++ load.py
@@ -1,1 +1,15 @@
-print("Uwu")
+# We can reflection variables.
+
+def setup():
+  """first run function"""
+  pass
+def first_setup():
+  """new client setup function"""
+  pass
+def loop():
+  """run every time function"""
+  pass
+def whitelist():
+  """run if whitelist"""
+  pass
+

